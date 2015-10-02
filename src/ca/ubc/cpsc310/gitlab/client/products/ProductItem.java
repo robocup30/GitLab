218cf7c9d82f130b2221134ed339e28e61c1d750
Joseph Choi <robocup30@hotmail.com>
2015-10-02 01:33:30 -0700

    Initial commit

---
+++ src/ca/ubc/cpsc310/gitlab/client/products/ProductItem.java
@@ -0,0 +1,14 @@
+package ca.ubc.cpsc310.gitlab.client.products;
+
+import java.io.Serializable;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public class ProductItem implements IsSerializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3724892846844204660L;
+
+}
---
+++ src/ca/ubc/cpsc310/gitlab/client/user/IUser.java
@@ -0,0 +1,32 @@
+package ca.ubc.cpsc310.gitlab.client.user;
+
+import java.io.Serializable;
+import java.util.List;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+import ca.ubc.cpsc310.gitlab.client.products.ProductItem;
+
+public interface IUser extends IsSerializable {
+
+	public String getLanguage();
+	
+	public String getName();
+	
+	public List<ProductItem> getWishList();
+	
+	public List<ProductItem> getShoppingCart();
+	
+	public void setLanguage(String language);
+	
+	public void setName(String name);
+	
+	public void addItemToWishList(ProductItem o);
+	
+	public void addItemToShoppingCart(ProductItem o);
+	
+	public void removeItemFromWishList(ProductItem o);
+	
+	public void removeItemFromShoppingCart(ProductItem o);
+	
+}
