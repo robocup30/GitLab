8efdac0b6421c60ca5a3216ebef0b7147ab67008
Joseph Choi <robocup300@hotmail.com>
2015-10-02 02:27:05 -0700

    First commit from second account

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -61,7 +61,7 @@ public class GitLab implements EntryPoint {
 			@Override
 			public void onSuccess(List<IUser> result) {
 				displayUsers(result);
-				//Window.alert("Got list back with " +  result.size() + " entries");
+				Window.alert("Got list back with " +  result.size() + " entries");
 				
 			}});
 		
