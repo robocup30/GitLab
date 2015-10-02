d4eba10f407a7de2a1b016cb23fcbce774ef533a
Joseph Choi <robocup30@hotmail.com>
2015-10-02 02:35:04 -0700

    Trying to cause conflict for second account

--- src/ca/ubc/cpsc310/gitlab/client/GitLab.java
+++ src/ca/ubc/cpsc310/gitlab/client/GitLab.java
@@ -54,8 +54,7 @@ public class GitLab implements EntryPoint {
 
 			@Override
 			public void onFailure(Throwable caught) {
-					Window.alert("Error occured " + caught.getClass() + " : " + caught.getMessage());
-				
+					Window.alert("altering message to cause conflict");
 			}
 
 			@Override
